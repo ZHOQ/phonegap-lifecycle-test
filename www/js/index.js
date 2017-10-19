0b259a1e6e939a65764b79802a656822c1318c4e
ZHOQ <ZHOQ1_15@UNI.WORC.AC.UK>
2017-10-19 16:11:42 +0100

    json

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,45 @@
-var key = "pen";
-var value = "blue";
-window.localStorage.setItem(key, value);
-window.alert(key);
+
+
+
+localStorage.setItem("pen", "blue");
+var val = localStorage.getItem("pen");
+
+localStorage.setItem("pen", "pink");
+var val1 = localStorage.getItem("pen");
+
+    var person= {
+		"firstName": "Homer",
+  		"lastName": "Simpson",
+ 		"address": {
+    			"streetAddress": "742 Evergreen Terrace",
+    			"city": "Springfield",
+    			"state": "?"
+		}
+	}
+    
+
+
+console.log("name: "+ person.firstName);
+    console.log("city: " + person.address.city);
+    
+    var personString = JSON.stringify(person);
+
+localStorage.setItem("address",personString);
+
+var getPerson = window.localStorage.getItem("address");
+
+var personJSON = JSON.parse(getPerson);
+
+
+    
+$(document).ready(function() {
+    console.log("ready!");
+    $("#key").text("pen: " + val);
+    $("#key1").text( "pen: " + val1);
+    $("#person").text("name: " + personJSON.firstName);
+
+
+
+
+
+});
